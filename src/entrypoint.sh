ee1b1d3e54bf49dbfbcc5f8059298b5aba15631e
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-06-29 14:06:59 +0200

    devtools installation of RcppDeepState

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "RcppDeepState"
-
+### Dependencies installation
+Rscript -e 'install.packages("devtools", repos="https://cloud.r-project.org")'
+Rscript -e 'devtools::install_github("FabrizioSandri/RcppDeepState")'
