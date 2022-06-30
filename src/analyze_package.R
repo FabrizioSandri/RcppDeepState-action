d8a42bbc11c688f16637b640f5becb7b36e10987
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-06-30 14:10:29 +0200

    run RcppDeepState analysis

--- src/analyze_package.R
+++ src/analyze_package.R
@@ -1,2 +1,9 @@
 require(RcppDeepState)
 
+GitHub_workspace <- Sys.getenv("GITHUB_WORKSPACE")
+
+deepstate_harness_compile_run(GitHub_workspace)
+result <- deepstate_harness_analyze_pkg(GitHub_workspace)
+
+print(result)
+print(result$logtable)
