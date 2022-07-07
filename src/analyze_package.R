4d02ac2ec894606873aa44f82471a1cc5952abd0
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-07-07 09:55:11 +0200

    getErrors auxiliary function

--- src/analyze_package.R
+++ src/analyze_package.R
@@ -6,5 +6,16 @@ location <- Sys.getenv("INPUT_LOCATION")
 deepstate_harness_compile_run(file.path(GitHub_workspace, location))
 result <- deepstate_harness_analyze_pkg(file.path(GitHub_workspace, location))
 
+
+# Auxiliary function used to get the number of errors for a single file that has
+# been analyzed. Each "logtableElement" correspond to a "data.table" instance 
+# where the second dimension describes the number of columns, whereas the second 
+# describes the number of errors found.
+getErrors <- function(logtableElement){
+    return (dim(vec)[1])
+}
+
+
+
 print(result)
 print(result$logtable)
