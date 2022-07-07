bea7b52afdb2e4b2446d2af00e83df9a3a5a9957
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-07-07 10:09:59 +0200

    exit with the proper status code

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -9,8 +9,12 @@ mkdir -p ~/.R
 echo -e "CXXFLAGS = \nCXX11FLAGS = \nCXX14FLAGS = \nCXX17FLAGS = \nCXX20FLAGS = \n" > ~/.R/Makevars
 
 ### Start the analysis
-echo "RcppDeepState analysis started"
+echo "RcppDeepState analysis started..."
 
 Rscript "/analyze_package.R"
 
 echo "RcppDeepState analysis completed"
+
+# get the exit status code of the analysis and return it to the action
+retVal=$?
+exit $retVal
