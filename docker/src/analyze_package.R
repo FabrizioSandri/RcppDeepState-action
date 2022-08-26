cd25eda0697064d821a48adcd00b4f985507a791
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2022-08-26 20:37:10 +0200

    na rm set to true

--- docker/src/analyze_package.R
+++ docker/src/analyze_package.R
@@ -56,7 +56,7 @@ getErrors <- function(logtableElement) {
 # Auxiliary function used to get the number of inputs that generated errors for
 # a given batch of analysis results
 getErrorsCount <- function(batch){
-	sum(lapply(batch, nrow) > 0)
+	sum(unlist(lapply(batch, nrow)) > 0, na.rm = TRUE)
 }
 
 getFunctionName <- function(test_path) {
